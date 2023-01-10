4d30cde2b5049d34f3bfdc595ffe95c4dd8790e7
Vijay Sam <vijay203.vk@gmail.com>
2023-01-10 22:53:31 +0530

    Added Form validations to both forms

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,31 +1,70 @@
 import React, { useState } from "react";
 import "./Login.css";
+import axios from "axios";
 
 function LoginForm() {
-  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false); // will be used later to goto homepage
   const submitForm = (e) => {
     e.preventDefault();
+    setFormErrors(validate(email, password));
+  };
+  const validate = (email, password) => {
+    var flag = true;
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!email) {
+      errors.email = "Email is required!";
+      flag = false;
+    } else if (!regex.test(email)) {
+      errors.email = "This is not a valid email format!";
+      flag = false;
+    }
+    if (!password) {
+      errors.password = "Password is required";
+      flag = false;
+    } else if (password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+      flag = false;
+    } else if (password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
+      flag = false;
+    }
+    if (flag) {
+      loginUser({ email, password }).then(() => {
+        setIsSubmit(true);
+      });
+    }
+    return errors;
+  };
+  const loginUser = (formValues) => {
+    return axios
+      .post("http://localhost:8080/check", formValues)
+      .then((response) => response.data);
   };
   return (
     <div className="login">
       <div>
         <h2 className="hero1">Login</h2>
         <p className="hero2">Login to continue.</p>
-        <form onSubmit={submitForm} autoComplete="off">
+        <form onSubmit={submitForm} autoComplete="off" noValidate>
           <div className="inputbox">
             <div className="userinput">
               <p>Email:</p>
+              <p className="err">{formErrors.email}</p>
               <input
                 required
                 id="email"
                 type="email"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               ></input>
             </div>
             <div className="userinput">
               <p>Password:</p>
+              <p className="err">{formErrors.password}</p>
               <input
                 required
                 id="password"
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,24 +1,91 @@
 import React, { useState } from "react";
 import "./Login.css";
 import "./Signup.css";
+import axios from "axios";
 
 function LoginForm() {
+  const auth = {};
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmpassword, setConfirmPassword] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [message, setMessage] = useState("");
   const submitForm = (e) => {
     e.preventDefault();
+    setFormErrors(validate(name, email, password, confirmpassword));
+    setIsSubmit(true);
+  };
+  const validate = (username, email, password, confirmPassword) => {
+    var flag = true;
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!username) {
+      errors.name = "Username is required!";
+      flag = false;
+    }
+    if (!email) {
+      errors.email = "Email is required!";
+      flag = false;
+    } else if (!regex.test(email)) {
+      errors.email = "This is not a valid email format!";
+      flag = false;
+    }
+    if (!password) {
+      errors.password = "Password is required";
+      flag = false;
+    } else if (password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+      flag = false;
+    } else if (password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
+      flag = false;
+    }
+    if (!(password === confirmPassword)) {
+      errors.confirmPassword = "Passwords don't match";
+      flag = false;
+    }
+    if (flag) {
+      auth.name = name;
+    }
+    if (Object.keys(auth).length > 0) {
+      signinUser(auth).then((jwtTokenData) => {
+        setMessage(jwtTokenData.response);
+      });
+    }
+    if (message === "true") {
+      errors.userName = "Username already exists";
+    }
+    if (flag && message === "false") {
+      fetch("http://localhost:8080/save", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, password, confirmPassword }),
+      });
+    }
+    return errors;
+  };
+  const signinUser = (auth) => {
+    return axios
+      .post("http://localhost:8080/auth", auth)
+      .then((response) => response.data);
   };
   return (
     <div className="signup">
       <div>
         <h2 className="hero1">Sign Up</h2>
         <p className="hero2">Fill your details.</p>
-        <form onSubmit={submitForm} autoComplete="off">
+        <form onSubmit={submitForm} autoComplete="off" noValidate>
           <div className="inputbox">
+            {Object.keys(formErrors).length === 0 && isSubmit ? (
+              <div className="success">Signed up successfully</div>
+            ) : (
+              <></>
+            )}
             <div className="userinput">
               <p>Username:</p>
+              <p className="err">{formErrors.name}</p>
               <input
                 required
                 id="name"
@@ -29,6 +96,7 @@ function LoginForm() {
             </div>
             <div className="userinput">
               <p>Email:</p>
+              <p className="err">{formErrors.email}</p>
               <input
                 required
                 id="email"
@@ -39,6 +107,7 @@ function LoginForm() {
             </div>
             <div className="userinput">
               <p>Password:</p>
+              <p className="err">{formErrors.password}</p>
               <input
                 required
                 id="password"
@@ -49,6 +118,7 @@ function LoginForm() {
             </div>
             <div className="userinput">
               <p>Retype your password:</p>
+              <p className="err">{formErrors.confirmPassword}</p>
               <input
                 required
                 id="confirmpassword"
